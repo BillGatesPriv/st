e35603c6145ef81898cbc7fa8fc68018ec6210b8
coalbl4ck <yannik.lorenz.1999@gmail.com>
2022-04-18 22:00:40 +0200

    config.h: Increased font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=13:antialias=true:autohint=true" };
 static int borderpx = 2;
 
